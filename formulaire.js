ac62645193dd1dbf56ed9794a94b193f6ea865d1
oudinyannick <yannickoudindu49@gmail.com>
2024-04-15 16:49:22 +0200

    modification des image et amélioration des pages

---
+++ formulaire.js
@@ -0,0 +1,12 @@
+document.getElementById('monFormulaire').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+  
+    
+    let monNom = document.getElementById('name').value;
+    let monPrenom = document.getElementById('userName').value;
+    let eMail = document.getElementById('eMail').value;
+    let numberTelephone = document.getElementById('telephone').value;
+    let message = document.getElementById('text').value;
+  
+   
+  });
